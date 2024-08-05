4feee78becd2ada7252e7d2cdfe7fdb379ca514b
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2024-08-05 19:37:58 +0530

    worked on news letter

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import './NewsLetter'
+import './NewsLetter.css'
 const NewsLetter = () => {
   return (
     <div className='news-letter'>
-      
+      <h1>Get exclucive offer on email</h1>
+      <p>Subscribe to our news letter and stay update</p>
+      <div>
+        <input type="email" placeholder='Your E- Mail Id' />
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
